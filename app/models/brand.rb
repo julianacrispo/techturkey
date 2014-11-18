b29f4494fdb4b81c057a076e6e6932d7b8b80355
Juliana Crispo <julianacrispo@gmail.com>
2014-11-18 10:35:08 -0500

    updated models to fix image error

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,5 @@
 class Brand < ActiveRecord::Base
+  attr_accessor :image_file_name, :image_content_type, :image_file_size, :image_updated_at
   has_many :customers
   has_many :vendors, through: :customers
 

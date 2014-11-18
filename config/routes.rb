b411b76c9904c5d24f2e9999331b00c64a91c14c
Juliana Crispo <julianacrispo@gmail.com>
2014-11-17 22:15:00 -0500

    updated gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :vendors
-
   resources :brands
+
+  root 'brands#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
